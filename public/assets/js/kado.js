0fefcaf7aa67553477f7995d3b0e47df73a39d47
Alex Fernandez <alexfdz@gmail.com>
2015-07-02 01:49:29 +0100

    Pass Url path as username to store

--- public/assets/js/kado.js
+++ public/assets/js/kado.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
     $('form[name=delivery-details]').submit(function(e){
-        $.post($(this).attr('action'), $(this).serialize(), function(res){});
+        $.post($(this).attr('action'), $(this).serialize() + '&' + userParameter());
         $('#form-container').hide();
         $("#thanks").removeClass('hidden');
         return false; 
     });     
-});         
+});      
+   
+function userParameter() {
+    return 'entry.1926467036=' + location.pathname;
+}

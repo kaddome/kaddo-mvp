ae0ad73f9b410cea04117cb9e250a84952ea853f
Alex Fernandez <alexfdz@gmail.com>
2015-07-02 22:46:55 +0100

    no store info

--- public/assets/js/kado.js
+++ public/assets/js/kado.js
@@ -1,5 +1,30 @@
 
 $(document).ready(function() {
+
+    $("#firstName").change(function(){
+        reportEventToGA('firstNameEntered', 'user has entered the firstName field')
+    });		
+    
+    $("#surname").change(function(){
+        reportEventToGA('surnameEntered', 'user has entered the surname field')			
+    });
+    
+    $("#town").change(function(){
+        reportEventToGA('townEntered', 'user has entered the town field')			
+    });
+    
+    $("#propertyNumberAndAddress").change(function(){
+        reportEventToGA('propertyNumberAndAddressEntered', 'user has entered the property number and address field')			
+    });
+    
+    $("#postcode").change(function(){
+        reportEventToGA('postcodeEntered', 'user has entered the postcode field')
+    });
+    
+    $(".btn.btn-primary").click(function(){
+        reportEventToGA('deliveryDetailsFormSubmitted', 'user has submitted the DD form')
+    });
+		
     var notEmptyConfig = {message: 'Is required and can\'t be empty'};
     $('#delivery-details')
         .bootstrapValidator({
@@ -10,24 +35,24 @@ $(document).ready(function() {
                 validating: 'glyphicon glyphicon-refresh'
             },
             fields: {
-                'entry.1721080211': {
+                firstName: {
                     validators: { notEmpty: notEmptyConfig }
                 },
-                'entry.548265541': {
+                surname: {
                     validators: { notEmpty: notEmptyConfig }
                 },
-                'entry.1091746689': {
+                town: {
                     validators: { notEmpty: notEmptyConfig }
                 },
-                'entry.15437857': {
+                propertyNumberAndAddress: {
                     validators: {  notEmpty: notEmptyConfig }
                 },
-                'entry.1075178930': {
+                postcode: {
                     validators: {
                         notEmpty: notEmptyConfig ,
                         zipCode: {
                             country: 'GB',
-                            mekssage: 'Is not a valid post code'
+                            message: 'Is not a valid post code'
                         }
                     }
                 }
@@ -38,7 +63,7 @@ $(document).ready(function() {
             var $form = $(e.target);
             var bv = $form.data('bootstrapValidator');
                     
-            $.post($(this).attr('action'), $(this).serialize() + '&' + userParameters());
+            $.post($(this).attr('action'), userParameters());
             $('#form-container').hide();
             $("#thanks").removeClass('hidden');
         });
@@ -48,9 +73,19 @@ function userParameters() {
     var data = window.atob(location.pathname.substring(1));
     var parameters = data.split(',');
     
-    var userParameters =  'entry.1926467036=' + parameters[0];
+    var userParameters =  'entry.1926467036=\'' + parameters[0];
     if(parameters.length > 1){
         userParameters = userParameters + '&entry.1904787477=' + parameters[1];
     }
     return userParameters;
+}
+
+function reportEventToGA(eventAction, eventLabel){
+    ga('send', 
+            'event', 
+            'DeliveryDetailsForm7', 
+            eventAction, 
+            eventLabel, {
+            page: 'Delivery Details Form - LOCALHOST - TESTING'
+    });
 }
